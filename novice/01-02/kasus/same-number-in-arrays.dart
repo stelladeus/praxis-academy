6acf05e215c530841da587dce659ea4e43ca15b9
Iqbaldhia Aghnadiin <r13.aero@yahoo.co.id>
2019-09-24 16:32:15 +0700

    Update same-number-in-arrays.dart

--- novice/01-02/kasus/same-number-in-arrays.dart
+++ novice/01-02/kasus/same-number-in-arrays.dart
@@ -1,5 +1,5 @@
 main(){
-var ArrayA = [1,2,3,4,5,6,7];
+var ArrayA = [1,2,3,5,8,6,7];
 var ArrayB = [5,5,5,5,5,5,5];
 
 findmatch(ArrayA,ArrayB);
@@ -9,6 +9,6 @@ findmatch(ArrayA,ArrayB);
 void findmatch(List<int> ArrayA, List<int> ArrayB){
   for(var i = 0; i < ArrayA.length-1 ; i++)
     for(var j = 0; j<ArrayB.length-1; j++){
-      if(ArrayA[i] = ArrayB[j]) print(true);
+      if(ArrayA[i] == ArrayB[j]) print(true);
     }
 }
