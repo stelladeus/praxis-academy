ec17dfec506eac55f6fcc4112ddfc9c643584737
Iqbaldhia Aghnadiin <r13.aero@yahoo.co.id>
2019-09-25 06:19:01 +0700

    add file

---
+++ novice/01-02/kasus/index-equal-to-value.dart
@@ -0,0 +1,25 @@
+// index equal to value menggunakan binary sort pada array yang sudah di sort sebelumnya
+// belum selesai
+
+void main() {
+  var array = new List();
+  array = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12]; //array di sort terlebih dahulu
+  var result = indexToValue(array, 0, array.length - 1);
+  print('Hasil :' + result.toString());
+}
+
+double indexToValue(List array, var init, var end) {
+  var mid = 0;
+  for (init = 0; init < end; init++) {
+    if (init > end) return -1;
+
+    double mid = (init + end) / 2;
+    print(mid);
+    if (array[mid.toInt()] >= mid)
+      return indexToValue(array, init, mid - 1);
+    else if (array[mid.toInt()] < mid)
+      return indexToValue(array, mid + 1, end);
+    else
+      return mid;
+  }
+}
