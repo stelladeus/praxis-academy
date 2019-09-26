73f4f2a02887f6b01633a6ab7c89e894dbaecc05
Iqbaldhia Aghnadiin <r13.aero@yahoo.co.id>
2019-09-26 13:51:30 +0700

    Update latihan-dart-menggunakan-oop.dart

--- novice/01-03/latihan/latihan-dart-menggunakan-oop.dart
+++ novice/01-03/latihan/latihan-dart-menggunakan-oop.dart
@@ -1,21 +1,24 @@
 main(List<String> args) {
-  Dog d = Dog('Darky',9,'Chihuahua', 'black');
+  // Dog d = Dog('Darky',9,'Chihuahua', 'black');
   // Dog d = Dog(name: 'Darker', age: 9,breed: 'Chihuahua',color: 'black');
+  // Animal c = Animal(2);
+
   // Animal a = Animal(name: 'cobaNama',age: 21);
-  Animal b = Animal('cobaNama',21);
+  // Animal b = Animal('cobaNama',21);
   // d.bark();
   // print(d.toString());
   //();
 }
- 
+
 class Animal {
   String name;
   int age;
- 
-  // Animal({this.name, this.age});
-  Animal({name: this.name,age: this.age});
-  Animal(this.name,this.age);
- 
+  int foot;
+
+  Animal(this.name, this.age);
+  // Animal(name: this.name,age: this.age);
+  // Animal(this.foot);
+
   Animal.newBorn() {
     name = 'Doggy';
     age = 0;
